876e6d4140196fbe32200f75869e041cca2b7f04
Mick Koch <mick@kochm.co>
2018-01-01 20:56:21 -0500

    Fix typo

--- src/lib.rs
+++ src/lib.rs
@@ -46,7 +46,7 @@
 //! ## Error Accumulation
 //!
 //! Due to the accumulation of floating-point roundoff errors, the accuracy of returned
-//! sine/cosine evaulations will slowly degrade over phase steps. Using a very small phase
+//! sine/cosine evaluations will slowly degrade over phase steps. Using a very small phase
 //! step or running an `IQOsc` through many, many cycles will make this problem more
 //! pronounced. As a workaround, the double-precision `IQOsc<f64>` can be used, which
 //! provides a significant increase in accuracy across phase steps and has relatively
